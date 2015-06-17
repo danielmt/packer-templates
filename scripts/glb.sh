35d3a6675d5404d23bfd9a73acbbcaaa1e4dcd21
Daniel Mascarenhas <daniel.tavares@corp.globo.com>
2015-06-17 16:44:02 -0300

    removing debian 7 (wheezy) templates

---
+++ scripts/glb.sh
@@ -0,0 +1,21 @@
+# system utils
+apt-get -y install curl unzip git sudo sysv-rc-conf tzdata
+apt-get -y install most htop vim ack-grep silversearcher-ag
+
+# crypto
+apt-get -y install gpgv2
+
+# java
+# apt-get -y install maven openjdk-7-jdk
+
+# libs
+# apt-get -y install flex libmysqlclient-dev libcurl3-dev libcurl4-gnutls-dev
+# apt-get -y install libgeos-dev libxml2-dev libxslt-dev
+# apt-get -y install libjpeg-dev liblcms2-dev libwebp-dev libgd2-noxpm-dev
+# apt-get -y install libgcrypt11-dev libgcrypt20-dev libldap2-dev libidn11-dev librtmp-dev libssh2-1-dev comerr-dev libkrb5-dev
+
+
+# python specific
+# apt-get -y install python-dev python-pip python-imaging python-setuptools
+# apt-get -y install python3.4 python3.4-dev
+# apt-get build-dep python-imaging -q -y
