b078996a4f5ac831608b05d98f96f89e02b20f58
Daniel Mascarenhas <daniel.tavares@corp.globo.com>
2015-06-17 16:44:52 -0300

    upgrade puppet apt-repo to jessie

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -1,8 +1,8 @@
 # Prepare puppetlabs repo
-wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
-dpkg -i puppetlabs-release-wheezy.deb
+wget http://apt.puppetlabs.com/puppetlabs-release-jessie.deb
+dpkg -i puppetlabs-release-jessie.deb
 apt-get update
 
 # Install puppet/facter
 apt-get install -y puppet facter
-rm -f puppetlabs-release-wheezy.deb
+rm -f puppetlabs-release-jessie.deb
